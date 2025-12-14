7e8edc52fbb0cd5ff48ebaa714015c2af65d7293
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-14 21:27:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let big = 0;
+function bigson(a,b,c) {
+    if (bigson % 2 == 0) {
+     big++ 
+    }
+}
+console.log(12,3,45);
+
+//5---
+let n = 5;
+function top(n) {
+    if(n % 2 == 0) {
+        console.log("musbat");
+        
+    }else if(n /2 == 1) {
+        console.log("nol");
+        
+    }else{
+        console.log("manfiy");
+        
+    }
+}
+
+//6----
+let start = 22;
+let end = 15;
+function juft_sonlar (start,end) {
+    for(let i= 0; i < juft_sonlar;i++){
+        if(start % 2 == 0) {
+            console.log("just son",i);
+            
+        }
+    }
+}
